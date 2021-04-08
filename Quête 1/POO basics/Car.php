a6fbb1c46153c8192565583d4fb2910053c33e34
Lang <78435873+Martin-Justin-Lang@users.noreply.github.com>
2021-04-08 17:53:01 +0200

    Add files via upload

---
+++ Quête 1/POO basics/Car.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once '../vehicule.php';
+
+class Car extends Vehicule
+{
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+private string $energy;
+
+private int $energyLevel;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+}
---
+++ Quête 1/POO basics/index.php
@@ -0,0 +1,56 @@
+<?php
+
+echo "<h1>Exercice Bike </h1>";
+
+require_once 'Bicycle.php';
+$bike = new Bicycle('yellow', 2);
+echo $bike->forward();
+var_dump($bike);
+
+
+echo $bike->forward();
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bike->brake();
+echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bike->brake();
+
+
+// Instanciation d'un nouvel objet $rockrider
+$rockrider = new Bicycle('yellow', 2);
+
+// Instanciation d'un nouvel objet $tornado
+$tornado = new Bicycle('black',2);
+
+$tornado->forward();
+
+Echo "<h1>Challenge car</h1>";
+
+require_once 'Car.php';
+$car = new Car ('yellow', 4, 'fuel');
+var_dump($car);
+
+echo $car->forward();
+echo '<br> Start: ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->forward();
+echo '<br> Vitesse de la voiture : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake();
+echo '<br> Vitesse du car : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake();
+
+var_dump(car::ALLOWED_ENERGIES);
+
+Echo "<h1>Challenge camion</h1>";
+
+require_once 'Camion.php';
+$camion = new Camion('red',5, 'diesel', 500, 18000);
+var_dump($camion);
+
+var_dump(Camion::ALLOWED_ENERGIES);
+
+
+echo "Le camion à une couleur " .$camion->getColor() ." , un nombre de  " .$camion->getNbSeats() ." places " . "et roule en " .$camion->getEnergy()."<br/>";
+echo "La capacité maxi de son chargement est de " .$camion->getStorageCapacity() . " tonnes"."<br/>";
+echo $camion->setLoad(50);
+echo "Le camion est chargé à " .$camion->getLoad() ." tonnes" ." il est donc en " .$camion->camionIsFull() ."<br/>";
+echo $camion->setLoad(100);
+echo "Le camion est chargé à " .$camion->getLoad() ." tonnes" ." il est donc en " .$camion->camionIsFull();
